d17b12214d19891bb941d7f8bba3d862d1038aac
Piyush Prateek <piyushprateek231996@gmail.com>
2022-12-11 15:52:05 +0530

    VideoHubProject

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -71,25 +71,25 @@ const MyCarousel = () => (
     showArrows={false}
   >
     <Box w="full" h={'100vh'}>
-      <Image src={img1} />
+      <Image src={img1} h={'full'} w={'full'} objectFit={'cover'} />
       <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
         Watch The Future
       </Heading>
     </Box>
     <Box w="full" h={'100vh'}>
-      <Image src={img2} />
+      <Image src={img2} h={'full'} w={'full'} objectFit={'cover'} />
       <Heading bgColor={'whiteAlpha.900'} color={'black'} {...headingOptions}>
         Future Is Gaming
       </Heading>
     </Box>
     <Box w="full" h={'100vh'}>
-      <Image src={img3} />
+      <Image src={img3} h={'full'} w={'full'} objectFit={'cover'} />
       <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
         Gaming On Console
       </Heading>
     </Box>
     <Box w="full" h={'100vh'}>
-      <Image src={img4} />
+      <Image src={img4} h={'full'} w={'full'} objectFit={'cover'} />
       <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
         Night Life Is Cool
       </Heading>
